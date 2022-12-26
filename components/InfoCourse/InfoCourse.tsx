1335640369101da32bedf98ab9047f7633cd1377
Arthur Bernardo <arthurutschgg@gmail.com>
2022-12-26 13:09:30 -0300

    primeira versão

--- components/InfoCourse/InfoCourse.tsx
+++ components/InfoCourse/InfoCourse.tsx
@@ -1,19 +1,8 @@
-import {use} from 'react'
-async function getCourse(id: number) {
-  const res = await fetch(
-    `https://my-json-server.typicode.com/ArthurBernardoDev/json_server/course/${id}`,
-    { next: {revalidate: 86400}}
-  );
-  return res.json();
-}
-
 type infoCourseProps = {
-  id: string
-}
+  id: string;
+};
 
 export const InfoCourse = ({ id }: infoCourseProps) => {
-  const course = use(getCourse(Number(id)));
-
   return (
     <>
       <div className={"flex-col xl:p-1   mb-10"}>
@@ -21,29 +10,29 @@ export const InfoCourse = ({ id }: infoCourseProps) => {
           <header className={"text-center mb-5"}>
             <img
               className={"rounded-t-lg"}
-              src={course.image}
+              src="https://i.ytimg.com/vi/C2HU3AY7IkU/maxresdefault.jpg"
               width={400}
               height={400}
             />
           </header>
           <div>
             <h1 className={"text-sm font-black ml-4 text-blue_900 mx-6"}>
-              {course.name}
+              curso nextjs
             </h1>
             <div>
               <p className={"text-xs font-normal ml-4 mt-3  w-80 xl:w-96"}>
-                {course.description}
+                descrição do curso
               </p>
             </div>
           </div>
           <div className={"pb-6"}>
             <div className={"pr-32"}>
               <p className={"font-bold text-sm text-blue_900 ml-4 mt-1"}>
-                R${course.price}
+                R$ 900
               </p>
 
               <p className={"text-xs font-semibold text-blue_900 ml-4 mt-1"}>
-                ou 9x de R${String(course.price / 9).substring(0, 5)}
+                ou 9x de R${String(100 / 9).substring(0, 5)}
               </p>
               <p className={"font-bold xl:text-2xl text-green_900 ml-4 mt-1"}>
                 s/ juros{" "}
